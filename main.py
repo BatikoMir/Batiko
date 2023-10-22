6f8d9a6c679bca8b9b1c144d22e5bc3f495fefd9
BatikoMir <148719864+BatikoMir@users.noreply.github.com>
2023-10-22 23:29:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-#Knacc главного окна
+from tkinter import ttk
+
+#Kлacc главного окна
 class Main(tk.Frame):
     def __init_(self, root):
         super().__init__(root)
@@ -9,6 +11,31 @@ class Main(tk.Frame):
     def init_main(self):
         toolbar = tk. Frame(bg=' #d7d7d7',bd=2)
         toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        # Открытие нового окна по кнопке Добавить
+        self.add_img = tk.PhotoImage(file='./img/add-png')
+        btn_add = tk.Button(toolbar, bg='#d7d7d7', bd=0, 
+                            image=self.add_img, command=self.open_child)
+        btn_add. pack (side=tk.LEFT)
+
+        # Добавляем таблицы
+        self.tree = ttk.Treeview(self, columns= ('ID', 'name' , 'phone' , 'email'),
+                                height=45, show="headings")
+        
+        # Добавить параметры колонкам
+        self.tree.column('ID', width=45, anchor=tk.CENTER)
+        self.tree.column('name', width=300, anchor=tk.CENTER)
+        self.tree.column ('phone', width=150, anchor=tk.CENTER)
+        self.tree.column('email', width=150, anchor=tk.CENTER)
+
+        # Подписи колонок
+        self.tree.heading("ID", text="ID")
+        self.tree.heading("name", text="ФИ0")
+        self.tree.heading("phone", text="Телефон")
+        self.tree.heading ("email", text="E-mail")
+
+        #Упаковка
+        self.tree.pack(side=tk.LEFT)
 
     def open_child(self):
         Child()
@@ -21,9 +48,45 @@ class Child(tk.Toplevel):
 
     # Создание и работа с дочерним окном
     def init_child(self):
-        self.title('Добавить')
+        self.title('Добавить контакт')
         self.geometry('400x200')
         # перехватываем все события происходящие в приложении
         self.grab_set ()
         # захватываем фокус
         self.focus_set()
+
+        label_name = tk.Label (self, text='ФИ0: ')
+        label_name. place(x=50, y=50)
+        label_phone = tk.Label(self, text= 'Телефон: ')
+        label_phone. place(x=50, y=80)
+        label_email = tk.Label(self, text='E-mail: ')
+        label_email. place(x=50, y=110)
+
+        self.entry_name = ttk. Entry(self)
+        self.entry_name.place(x=200, y=50)
+        self.entry_phone = ttk.Entry(self)
+        self. entry_phone.place(x=200, y=80)
+        self.entry_email = ttk.Entry(self)
+        self.entry_email. place (x=200, y=110)
+
+        # Кнопка закрытия
+        self.btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
+        self.btn_cancel.place(x=300, y=170)
+
+        # Кнопка добавления
+        self.btn_add = ttk.Button(self, text='Добавить')
+        self.btn_add.place(x=220, y=170)
+        self.btn_add.bind ('‹Button-1>')
+
+
+
+# Создание окна
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Main(root)
+    app.pack()
+    root.title('Телефонная книга')
+    root.geometry('645x450')
+    root.resizable(False,False)
+    root.configure('bg= White')
+    root.mainloop()
